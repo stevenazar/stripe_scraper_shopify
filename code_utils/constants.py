aa29c8d9db81f7f1dcdeb14992161ef35afcf680
stevenazar <62719262+stevenazar@users.noreply.github.com>
2024-04-21 19:42:37 +0200

    changes code another time

--- code_utils/constants.py
+++ code_utils/constants.py
@@ -1,6 +1,6 @@
 from decouple import config
 
-DOMAIN = 'http://localhost:8501' # TODO: update this url to the deployed url
+DOMAIN = 'http://scrapstore.onrender.com' # TODO: update this url to the deployed url
 SIGNUP_URL = f"{DOMAIN}/Sign_Up"
 LOGIN_URL = f"{DOMAIN}/ScrapStore"
 CHECKOUT_SUCCESS_URL = f"{DOMAIN}/ScrapStore"
